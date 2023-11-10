ebeace6f9674ec0919b14bf9776b1b995f4a5163
Malik Bagwala <m.bagwala@gmail.com>
2023-11-10 09:13:55 +0530

    feat: initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Board from "./components/board";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Board cols={5} rows={5} />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
