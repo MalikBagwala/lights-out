bc07016c5f8d14b3bc8c1c925c2305e9edace36a
Malik Bagwala <m.bagwala@gmail.com>
2023-11-10 09:42:02 +0530

    feat: add favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTransition, animated } from "@react-spring/web";
-
+import LogoSrc from "../assets/logo.png";
 const Logo = () => {
   const [toggle, setToggle] = useState(false);
 
@@ -14,10 +14,14 @@ const Logo = () => {
     <span style={{ cursor: "pointer" }}>
       {transitions((styles, item) => (
         <animated.span
-          style={{ ...styles, position: "absolute", top: 70, left: "49%" }}
+          style={{ ...styles, position: "absolute", top: 90, left: "49%" }}
           onClick={() => setToggle((prevToggle) => !prevToggle)}
         >
-          {item ? "⚡" : "✌"}
+          {item ? (
+            "⚡"
+          ) : (
+            <img src={LogoSrc} alt="logo" height={80} width={80} />
+          )}
         </animated.span>
       ))}
     </span>
